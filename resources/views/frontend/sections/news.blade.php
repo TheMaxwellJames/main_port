979cd12de7d7cfa89240dd275dbbeb8193220874
Maxwell James <maxichanj@gmail.com>
2023-09-14 10:29:35 +0100

    first commit

---
+++ resources/views/frontend/sections/news.blade.php
@@ -0,0 +1,20 @@
+<section class="portfolio-area section-padding-top"  id="news-page">
+       
+        <div class="">
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <h4>Join Our Newsletter</h4>
+                  <p>Get free daily programming news and update</p>
+                  <form action="#" method="post">
+                    <input type="email" name="email"><input type="submit" value="Subscribe">
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <br>
+          <br>
+          <br>
+</section>
